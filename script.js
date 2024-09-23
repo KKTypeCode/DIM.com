4a34101f6010c36aa94e3256a026728ac50c49b9
phenogood <74715388+phenogood@users.noreply.github.com>
2024-09-23 22:08:45 +0700

    Login box JS & HTML 3

--- script.js
+++ script.js
@@ -2,32 +2,52 @@ var sidemenu = document.getElementById('sidebar')
 const login = document.getElementById('signin')
 const inpuserid = document.getElementById('userid')
 const inppass = document.getElementById('password')
-const error = document.getElementById('error')
-var button = document.getElementById('enter')
+const error1 = document.getElementById('error1')
+const error2 = document.getElementById('error2')
+const error3 = document.getElementById('error3')
+const option = document.querySelectorAll('input[type="radio"][name="choice"]')
 
-"Sidebar"
+let selected = null
 
-"Login Form"
-form.addEventListener('enter', (e) => {
-    let msg = [] 
-    if (inpuserid.value == null || inpuserid.value === '') {
-        msg.push('Username/Email is required')
-    } else if (inpuserid.length > 0) {
-        e.preventDefault()
-    }
-    
-    if (inppass.value == null || inppass.value === '') {
-        msg.push('Password is required')
-    } else if (inppass.length > 0) {
-        e.preventDefault()
-        error.innerText = msg.join(', ')
-    }
-
-    if (inppass.length < 8) {
-        msg.push('Password is not long enough')
-        e.preventDefault()
-    }
+option.forEach((radio) => {
+    radio.addEventListener('click', (e) => {
+        if (selected === e.target) {
+            radio.checked = false
+            selected = null
+        } else {
+            selected = e.target
+        }
+    })
 })
 
-"reminder" = "the warning message in the addEventListener doesnt work"
-"missing_parts" = "for register/sign up page & sidemenu"
+login.addEventListener('submit', (e) => {
+    let msg1 = []
+    let msg2 = []
+    let msg3 = []
+
+    if (inpuserid.value.trim() == null || inpuserid.value.trim() === '') {
+        msg1.push('Username/Email is required')
+        inppass.value = ''
+    }
+
+    if (inppass.value.trim() == null || inppass.value.trim() === '') {
+        msg2.push('Password is required')
+    } else if (inppass.value.length < 8) {
+        msg2.push('Password is not long enough')
+    } else if (inppass.value.length > 20) {
+        msg2.push('Invalid password')
+    }
+
+    if (selected === null) {
+        msg3.push('Select one option')
+    }
+
+    if ((msg1.length + msg2.length + msg3.length) === 0) {
+        window.location.href = 'home.html'
+    } else {
+        e.preventDefault()
+        error1.innerHTML = msg1
+        error2.innerHTML = msg2
+        error3.innerHTML = msg3
+    }
+})
