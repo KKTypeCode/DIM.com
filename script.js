d069bd88c6d8717c7d73c4c7d58286f180e49782
phenogood <74715388+phenogood@users.noreply.github.com>
2024-10-08 14:23:25 +0700

    Login works, Register doesn't

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var sidemenu = document.getElementById('sidebar')
 const login = document.getElementById('signin')
 const register = document.getElementById('signup')
-const username = document.getElementById('username')
 const inpuserid = document.getElementById('userid')
 const inppass = document.getElementById('password')
+const usernameup = document.getElementById('usernameup')
+const mailup = document.getElementById('useridup')
+const passup = document.getElementById('passwordup')
 const error1 = document.getElementById('error1')
 const error2 = document.getElementById('error2')
 const error3 = document.getElementById('error3')
 const error4 = document.getElementById('error4')
+const error5 = document.getElementById('error5')
+const error6 = document.getElementById('error6')
 const option = document.querySelectorAll('input[type="radio"][name="choice"]')
 const remail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
@@ -31,16 +35,12 @@ login.addEventListener('submit', (e) => {
 
     if (remail.test(inpuserid.value) === false) {
         inppass.value = ''
-        if (inpuserid.value.trim() == null || inpuserid.value.trim() === '') {
+        if (inpuserid.value.trim() === null || inpuserid.value.trim() === '') {
             msg1.push('Username/Email is required')
-        } else {
-            msg1 = null
         }
-    }  else if (remail.test(inpuserid.value) === true) {
-        msg1 = null
     }
 
-    if (inppass.value.trim() === null || inppass.value.trim() === '') {
+    if (inppass.value.trim() === '') {
         msg2.push('Password is required')
     } else if (inppass.value.length < 8) {
         msg2.push('Password is not long enough')
@@ -53,51 +53,48 @@ login.addEventListener('submit', (e) => {
     }
 
     if ((msg1.length + msg2.length + msg3.length) === 0) {
-        window.location.href('signup.html')
+        console.log('Form submitted')
+        login.onsubmit()
     } else {
+        error1.innerHTML = msg1.join()
+        error2.innerHTML = msg2.join()
+        error3.innerHTML = msg3.join()
         e.preventDefault()
-        error1.innerHTML = msg1
-        error2.innerHTML = msg2
-        error3.innerHTML = msg3
     }
 })
-
-//warning for login page works when register.addEventListener is removed
-//but doesn't work when register.addEventListener is present in code
-//warning for register page still has not work
 
 register.addEventListener('submit', (e) => {
     let msg4 = []
     let msg5 = []
     let msg6 = []
-    let msg7 = []
 
-    if (username.value.trim() == null || username.value.trim() === '') {
+    if (usernameup.value.trim() === null || usernameup.value.trim() === '') {
         msg4.push('Username is required')
     }
 
-    if (remail.test(inpuserid.value) === false) {
+    if (mailup.vlaue.trim() === null || mailup.value.trim() === '') {
         msg5.push('Email is required')
-        inppass.value = ''
-    }  else if (remail.test(inpuserid.value) === true) {
-        msg5 = null
+        passup.value = ''
+    } else if (remail.test(mailup.value) === false) {
+        msg5.push('Invalid email')
+        passup.value = ''
     }
 
-    if (inppass.value.trim() == null || inppass.value.trim() === '') {
+    if (passup.vlaue.trim() === null || passup.value.trim() === '') {
         msg6.push('Password is required')
-    } else if (inppass.value.length < 8) {
+    } else if (passup.value.length < 8) {
         msg6.push('Password is not long enough')
-    } else if (inppass.value.length > 20) {
+    } else if (passup.value.length > 20) {
         msg6.push('Invalid password')
     }
 
-    if ((msg4.length + msg5.length + msg6.length + msg7.legnth) === 0) {
-        console.log('entered')
-    } else {
+    if ((msg4.length + msg5.length + msg6.length) === 0) {
+        console.log('Form submitted')
+        register.onsubmit()
+    } else if ((msg1.length + msg2.length + msg3.length) >= 0) {
+        error4.innerHTML = msg4.join()
+        error5.innerHTML = msg5.join()
+        error6.innerHTML = msg6.join()
         e.preventDefault()
-        error1.innerHTML = msg1
-        error2.innerHTML = msg2
-        error3.innerHTML = msg3
-        error4.innerHTML = msg4
     }
 })
