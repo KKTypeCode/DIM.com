b1d22898f6f2b2012cbe2b52d983eb23dd634d4e
phenogood <74715388+phenogood@users.noreply.github.com>
2024-12-17 00:54:02 +0700

    signup additions & JS configurations

--- signup.js
+++ signup.js
@@ -2,15 +2,33 @@ const register = document.getElementById('signup')
 const usernameup = document.getElementById('usernameup')
 const mailup = document.getElementById('usermailup')
 const passup = document.getElementById('passwordup')
+const investor = document.getElementById('investors')
+const business = document.getElementById('businesses')
 const error4 = document.getElementById('error4')
 const error5 = document.getElementById('error5')
 const error6 = document.getElementById('error6')
+const error7 = document.getElementById('error7')
+const option = document.querySelectorAll('input[type="radio"]')
 const remail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+let selected = null
+
+option.forEach((radio) => {
+    radio.addEventListener('click', (e) => {
+        if (selected === e.target) {
+            radio.checked = false
+            selected = null
+        } else {
+            selected = e.target
+        }
+    })
+})
 
 register.addEventListener('submit', (e) => {
     let msg4 = []
     let msg5 = []
     let msg6 = []
+    let msg7 = []
 
     if ((usernameup.value.trim() === null) || (usernameup.value.trim() === '')) {
         msg4.push('Username is required')
@@ -32,10 +50,21 @@ register.addEventListener('submit', (e) => {
         msg6.push('Invalid password')
     }
 
+    if (selected === null) {
+        msg7.push('Select one option')
+    }
+
     if ((msg4.length + msg5.length + msg6.length) != 0) {
         error4.innerHTML = msg4.join()
         error5.innerHTML = msg5.join()
         error6.innerHTML = msg6.join()
+        error7.innerHTML = msg7.join()
         e.preventDefault()
+    } else {
+        if (investor.checked) {
+            window.location.href = 'dashboard_investor.html'
+        } else if (business.checked) {
+            window.location.href = 'dashboard_business.html'
+        }
     }
 })
